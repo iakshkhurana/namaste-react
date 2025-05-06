7d30b792ba2b98bf91fdf141b5f5c014a0d540a8
Aksh Khurana <khuranaaksh08@gmail.com>
2025-05-07 01:49:12 +0530

    ep04 started

---
+++ ep04/README.md
@@ -0,0 +1,5 @@
+Jab app banaoge, toh ek UI ka idea lelena
+Like ek page pe banalena![idea](image.png)
+
+19:51
+-1:54:31
---
+++ ep04/app.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+/** 
+ * Header
+ *  - Logo
+ *  - Nav Items
+ * Body
+ *  - Search
+ *  - RestaurantContainer
+ *      - Restaurant Card
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Address
+ *  - Contact
+*/
+
+const Header = () =>{
+    return (
+        <div className="header">
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStIxLpCJp2oHK5huR5CZ7i1Jvfx44UOPUlwg&s"/>
+            </div>
+            <div className
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <div class = "app">
+            // Header
+            // Body
+            // Footer
+        </div>
+    )
+}
