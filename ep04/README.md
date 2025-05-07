8bbc2defc9edd568b1533b7fafe0d449363fc254
Aksh Khurana <khuranaaksh08@gmail.com>
2025-05-07 22:26:54 +0530

    ep04 continued

--- ep04/README.md
+++ ep04/README.md
@@ -1,5 +1,4 @@
 Jab app banaoge, toh ek UI ka idea lelena
 Like ek page pe banalena![idea](image.png)
 
-19:51
--1:54:31
+30:07
--- ep04/app.js
+++ ep04/app.js
@@ -1,38 +1,55 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-/** 
+/**
  * Header
  *  - Logo
  *  - Nav Items
  * Body
  *  - Search
  *  - RestaurantContainer
- *      - Restaurant Card
+ *    - RestaurantCard
+ *      - Img
+ *      - Name of Restaurant, Star Rating, cuisine, delivery time
  * Footer
  *  - Copyright
  *  - Links
  *  - Address
  *  - Contact
-*/
+ */
 
-const Header = () =>{
-    return (
-        <div className="header">
-            <div>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStIxLpCJp2oHK5huR5CZ7i1Jvfx44UOPUlwg&s"/>
-            </div>
-            <div className
-        </div>
-    )
-}
+const Header = () => {
+  return (
+    <div className="header">
+      <div>
+        <img
+          src="https://png.pngtree.com/template/20191014/ourmid/pngtree-pin-food-delivery-map-location-delivery-logo-concept-image_318151.jpg"
+          alt="Logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 const AppLayout = () => {
-    return (
-        <div class = "app">
-            // Header
-            // Body
-            // Footer
-        </div>
-    )
-}
+  return (
+    <div className="root">
+      {/* Header */}
+      <Header />
+      {/* Body */}
+      {/* Footer */}
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
